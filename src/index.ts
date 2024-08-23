41b2d169d69feb697162ae9a6b96f50cb91b8d32
BriMus88 <brian.muscat@outlook.com>
2024-08-23 17:41:11 +0100

    change

---
+++ src/index.ts
@@ -0,0 +1,13 @@
+// src/index.ts
+import express from 'express';
+
+const app = express();
+const port = process.env.PORT || 8080;
+
+app.get('/', (req, res) => {
+  res.send('Hello World from TypeScript brian!');
+});
+
+app.listen(port, () => {
+  console.log(`Server is running on port ${port}`);
+});
